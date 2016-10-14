d9fa0d15c18fd26494873beeb4527a32bd1d4475
ShanthoshK <kkshanthosh@gmail.com>
2016-10-15 11:03:00 +1300

    Added basic accordion slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,14 @@
 $( document ).ready(function(){
   $(".button-collapse").sideNav();
+  $('#accordionSlider').ncs({
+    // animationSpeed: 400,
+    // indent: 'auto',
+    // resizeAdaptation: true,
+    // useTimerForResizeAdaptation: false,
+    // autoWidth: true,
+    // autoHeight: true,
+    // active: 0,
+    // activeOnClick: false,
+    // offset: 32
+  });
 });
